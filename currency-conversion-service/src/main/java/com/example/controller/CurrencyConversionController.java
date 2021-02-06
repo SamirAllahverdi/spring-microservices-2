b587e2f28e131a0a49eaa6ae690def7931e7dfe3
Samir Allahverdi <your_email@example.com>
2021-02-07 01:23:40 +0400

    --- adding naming server

---
+++ currency-conversion-service/src/main/java/com/example/controller/CurrencyConversionController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import com.example.entity.CurrencyConversion;
+import com.example.proxy.CurrencyExchangeProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    private final CurrencyExchangeProxy currencyExchangeProxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
+    /**
+     * http://localhost:8100/currency-converter/from/USD/to/INR/quantity/10
+     */
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.getExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+
+    }
+
+}
