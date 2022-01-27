c1c1d0685cffce99fd27bc1e1890b3f5620aae07
Samir Allahverdi <samir.a@crocusoft.com>
2022-01-27 23:22:51 +0400

    --- optimization in currency microservices

--- currency-conversion-service/src/main/java/com/example/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/controller/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.entity.CurrencyConversion;
 import com.example.proxy.CurrencyExchangeProxy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,31 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
+@RequiredArgsConstructor
 public class CurrencyConversionController {
 
     private final CurrencyExchangeProxy currencyExchangeProxy;
 
-    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
-        this.currencyExchangeProxy = currencyExchangeProxy;
-    }
-
     /**
      * http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10
      */
-
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity) {
-
         CurrencyConversion currencyConversion = currencyExchangeProxy.getExchangeValue(from, to);
 
-        return new CurrencyConversion(currencyConversion.getId(),
-                from, to, quantity,
-                currencyConversion.getConversionMultiple(),
-                quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
-
+        return CurrencyConversion.builder()
+                .id(currencyConversion.getId())
+                .from(from).to(to)
+                .quantity(quantity)
+                .conversionMultiple(quantity.multiply(currencyConversion.getConversionMultiple()))
+                .build();
     }
-
 }
--- currency-exchange-service/src/main/java/com/example/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/controller/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 
 import com.example.entity.CurrencyExchange;
 import com.example.repository.CurrencyExchangeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,17 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 public class CurrencyExchangeController {
 
-
     private final Environment environment;
-
     private final CurrencyExchangeRepository currencyExchangeRepository;
-
-    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository) {
-        this.environment = environment;
-        this.currencyExchangeRepository = currencyExchangeRepository;
-    }
 
     /**
      * http://localhost:8000/currency-exchange/from/USD/to/INR
@@ -31,17 +26,12 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from,
                                              @PathVariable String to) {
-//        CurrencyExchange currencyExchange = new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(50));
-        Optional<CurrencyExchange> currencyExchangeOpt = currencyExchangeRepository.findByFromAndTo(from, to);
-
-        if (currencyExchangeOpt.isEmpty()) {
-            throw new RuntimeException("Unable to find data for " + from + " to " + to);
-        }else {
-            CurrencyExchange currencyExchange = currencyExchangeOpt.get();
-            currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
-
-            return currencyExchange;
-        }
+        return currencyExchangeRepository.findByFromAndTo(from, to)
+                .map(ce -> {
+                    ce.setEnvironment(environment.getProperty("local.server.port"));
+                    return ce;
+                })
+                .orElseThrow(() -> new RuntimeException(String.format("Unable to find data for %s to %s", from, to)));
     }
 
 
