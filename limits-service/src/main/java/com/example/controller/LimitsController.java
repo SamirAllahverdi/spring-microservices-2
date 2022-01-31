963733561a0081783827c88b1b8793b83f48c188
Samir Allahverdi <a_nazim2007@mail.ru>
2022-02-01 00:39:56 +0400

    --- added zipkin & sleuth (distributed tracing system)

--- limits-service/src/main/java/com/example/controller/LimitsController.java
+++ limits-service/src/main/java/com/example/controller/LimitsController.java
@@ -3,10 +3,13 @@ package com.example.controller;
 import com.example.config.Configuration;
 import com.example.entity.Limits;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class LimitsController {
 
@@ -14,6 +17,7 @@ public class LimitsController {
 
     @GetMapping("/limits")
     public Limits getLimits() {
+        log.info("/limits");
         return Limits.builder()
                 .minimum(configuration.getMinimum())
                 .maximum(configuration.getMaximum())
